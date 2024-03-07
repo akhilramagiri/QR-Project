f8040d31541883c79212ab4dd328060a172ddc9f
Akhil Ramagiri <akhilramagiri11@gmail.com>
2024-03-07 11:22:26 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from 'inquirer'
+import qr from 'qr-image'
+import fs from 'fs'
+
+inquirer
+  .prompt([
+    {message: "Type in your URL",
+     name: "URL",
+    },
+  ])
+  .then((answers) => {
+    var url = (answers.URL);
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('resume_qr.png'));
+    fs.writeFile('resume_link.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+   })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+
